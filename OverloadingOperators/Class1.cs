4ae7ef5f7ecd548d9e358a5ce1408a2438a4190d
DallasLewis1996 <dallaslewis1996@gmail.com>
2017-04-26 23:08:05 -0600

    First commit.

---
+++ OverloadingOperators/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    public struct Fraction
+    {
+        private int denom;
+        public int WholeNumber { get; set; }
+        public int Denominator { get; set; } 
+        public int Numerator { get; set; } 
+
+        public Fraction(int whole = 0, int numerator = 0, int denominator = 1)
+        {
+
+        }
+
+        public static int GCD(int m, int n)
+        {
+            if (m == 0)
+                return n;
+            if (n == 0)
+                return m;
+
+            if(m > n)
+            {
+                return GCD(m % n, n);
+            }
+            else
+            {
+                return GCD(m, n % m);
+            }
+        }
+
+    }
+
+    
+    
+}
